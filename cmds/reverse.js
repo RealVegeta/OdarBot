2e89c00e5c4ffa3f0c2c451c6bb568cc08332e75
RealVegeta <67888700+RealVegeta@users.noreply.github.com>
2020-07-06 10:05:48 +0530

    Update reverse.js

--- cmds/reverse.js
+++ cmds/reverse.js
@@ -3,7 +3,7 @@ exports.run = function (bot, msg, args) {
     if (args.length < 1) {
         throw 'You must input text to be reversed!';
     }
-    msg.reply(args.join(' ').split('').reverse().join(''));
+    msg.reply(args.join(' ').slice(7).split('').reverse().join(''));
 
 
 }
